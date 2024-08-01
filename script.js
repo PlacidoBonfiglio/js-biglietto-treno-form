b803c0b76a538cf17e8f0cb751278cf7175b68a2
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-01 16:57:59 +0200

    preparatory phase

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log('JS OK');
 
+// 1. Retrieve the element of my interest from the DOM
+// 2. Prepare the variable with the cost of the ticket based on the kilometers driven
+// 3. Ask the user for the number of kilometers they want to travel
+// 4. Ask the user for his or her age
+// 5. Calculate the total price of the trip
+// 6. Apply discount based on age
+// 7. Insert the ticket price into the DOM
+
+// PREPARATORY PHASE
+const fullNameField = document.getElementById('fullname');
+const kilField = document.getElementById('kilometers');
+const generateBtn = document.getElementById('generate');
+
+
+// FASE GESTIONE EVENTI
