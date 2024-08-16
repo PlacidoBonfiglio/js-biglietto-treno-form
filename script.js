26caba83710bb60587da4cf0ab699ce90c72a469
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-16 18:56:38 +0200

    passenger name inner text

--- script.js
+++ script.js
@@ -9,12 +9,17 @@ console.log('JS OK');
 // 7. Insert the ticket price into the DOM
 
 // PREPARATORY PHASE
-// 1. Retrieve the element of my interest from the DOM
+// Retrieve the element of my interest from the DOM
+
+// FORM
 const form = document.querySelector('form');
 const fullNameField = document.getElementById('fullname');
 const kilometersField = document.getElementById('kilometers');
-const age = document.getElementById('age');
+const ageField = document.getElementById('age');
 const generateBtn = document.getElementById('generate');
+
+// TICKET
+const ticket = document.getElementById('ticket');
 const passengerName = document.getElementById('passenger-name');
 const offer = document.getElementById('offer');
 const carriage = document.getElementById('carriage');
@@ -24,9 +29,12 @@ const price = document.getElementById('price');
 // Kilometer Price
 const kilometerPrice = 0.21;
 
+// Rate
+let rateName = 'Regular rate';
+
 
 // FASE GESTIONE EVENTI
-// Passenger Name
+// clicco il bottone "Generate"
 generateBtn.addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -34,23 +42,33 @@ generateBtn.addEventListener('click', function (e) {
     const fullName = fullNameField.value.trim();
 
     //recupero input kilometri da percorrerre
-    const totalKilometers = kilometersField.value;
+    const totalKilometers = parseInt(kilometersField.value);
 
     //recupero dell'età
-    const passengerAge = age.value;
+    const passengerAge = ageField.value;
 
+    console.log('fullName', fullName);
+    console.log('totalKilometers', totalKilometers);
+    console.log('passengerAge', passengerAge);
 
-    // stampo
-    if (fullName) {
-        passengerName.innerText = fullName;
-        fullNameField.value = '';
+    // Calcolo del prezzo base 
+    let price = totalKilometers * kilometerPrice;
+
+    // Verifica sconto in base all'età
+    if (passengerAge === 'minor') {
+        price = price * 0.8;
+        rateName = 'Minor rate';
+    } else if ( passengerAge === 'senior') {
+        price = price * 0.6;
+        rateName = 'Senior Rate'
     }
 
-    if (totalKilometers) {
-        kilometersField.value = '';
-    }
+    console.log(price, rateName);
 
-}
-)
+    // Stampo Nome Completo
+    passengerName.innerText = fullName;
+    
+})
+
 
 
